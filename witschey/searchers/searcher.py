0caa692941010b15ea9e55496c596932a1bdb1c6
Jim Witschey <jim.witschey@gmail.com>
2014-09-18 16:43:11 -0400

    moves fancy junk to `Searcher.__new__`

--- witschey/searchers/searcher.py
+++ witschey/searchers/searcher.py
@@ -1,11 +1,21 @@
 from __future__ import division, unicode_literals
 
-from witschey.base import memo
+from witschey.base import memo, The
+
+
+print(The.to_str())
 
 class Searcher(object):
+
+    def __new__(cls, *args, **kwargs):
+        o = super(Searcher, cls).__new__(cls, *args, **kwargs)
+        o.spec = memo(**kwargs)
+
+        if hasattr(The, cls.__name__):
+            o.spec(getattr(The, cls.__name__))
+
+        return o
+
     def __init__(self, model, *args, **kw):
-        self.iterations = kw['iterations']
         self.model = model
-        kw.setdefault('seed', 123)
-        self.spec = memo(**kw)
 
