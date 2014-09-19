091c0bf750360efe91a60ea4708323ea28dd9eab
Jim Witschey <jim.witschey@gmail.com>
2014-09-18 23:20:12 -0400

    properly comments up what's going on

--- witschey/searchers/searcher.py
+++ witschey/searchers/searcher.py
@@ -2,17 +2,30 @@ from __future__ import division, unicode_literals
 
 from witschey.base import memo, The
 
+from datetime import datetime
 
 print(The.to_str())
 
 class Searcher(object):
 
     def __new__(cls, *args, **kwargs):
-        o = super(Searcher, cls).__new__(cls, *args, **kwargs)
-        o.spec = memo(**kwargs)
+        # construct our object
+        future_self = super(Searcher, cls).__new__(cls, *args, **kwargs)
 
-        if hasattr(The, cls.__name__):
-            o.spec(getattr(The, cls.__name__))
+        name = cls.__name__
+        # give our object a spec, initialized with searcher's name
+        # and the initialization time
+        future_self.spec = memo(searcher=name, initialized=datetime.now())
+
+        # if there are global options for this class in The
+        if hasattr(The, name):
+            # add them to the spec
+            o.spec(getattr(The, name))
+
+        # then, add the kwargs to the constructor call to spec.
+        # NB: this happens after adding options from The, so 
+        #     call-specific options override the globals
+        o.spec(**kwargs)
 
         return o
 
