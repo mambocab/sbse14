6694a9b0e07dd821718a044695741454430aa2a5
Jim Witschey <jim.witschey@gmail.com>
2014-09-22 20:57:14 -0400

    adds hw4 and outputs

---
+++ hw4/search.py
@@ -0,0 +1,40 @@
+from __future__ import division, print_function
+
+from datetime import datetime
+import random, time
+
+from witschey.models import Schaffer, Fonseca, Kursawe
+from witschey.models import ZDT1, ZDT3, Viennet3
+from witschey.searchers import SimulatedAnnealer, MaxWalkSat
+from witschey.log import NumberLog
+
+def run(r=20, seed=10, text_report=False):
+    print(datetime.now())
+    for klass in (Schaffer, Fonseca, Kursawe, ZDT1, ZDT3, Viennet3):
+        xtiles = []
+        print("\n", klass.__name__, sep='')
+        print('-' * 50)
+        for searcher in (SimulatedAnnealer, MaxWalkSat):
+            random.seed(seed)
+            n = NumberLog(max_size=None)
+            times = NumberLog(max_size=None)
+            print(searcher.__name__)
+            for _ in range(r):
+                start_time = time.clock()
+                s = searcher(klass())
+                out = s.run(text_report=text_report)
+                times += time.clock() - start_time
+                n += out.best
+            print(s.spec.to_str(sep=': '))
+            if text_report:
+                print(out.report)
+
+            print('Best: {: .4f}'.format(n.mean()))
+            print('total time: {:.3f}s'.format(times.total()),
+                'mean time: {:.3f}s'.format(times.mean()), sep='\t')
+            print(n.xtile(width=30), sep='\n')
+            print('\n')
+        print('=' * 50 + '\n', '=' * 50, sep='')
+
+if __name__ == '__main__':
+    run(r=1, seed=1, text_report=True)
