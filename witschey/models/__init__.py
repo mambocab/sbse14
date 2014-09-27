d0efaf7944a42b88cf06257ed9d9eae45aecdc0e
Jim Witschey <jim.witschey@gmail.com>
2014-09-26 22:15:17 -0400

    cleans up namespaces of witschey.{models,searchers}

--- witschey/models/__init__.py
+++ witschey/models/__init__.py
@@ -1,8 +1,9 @@
-from independent_variable import IndependentVariable
+from independent_variable import IndependentVariable; del independent_variable
+from schaffer import Schaffer; del schaffer
+from kursawe import Kursawe; del kursawe
+from fonseca import Fonseca; del fonseca
+from zdt1 import ZDT1; del zdt1
+from zdt3 import ZDT3; del zdt3
+from viennet3 import Viennet3; del viennet3
 
-from schaffer import Schaffer
-from kursawe import Kursawe
-from fonseca import Fonseca
-from zdt1 import ZDT1
-from zdt3 import ZDT3
-from viennet3 import Viennet3
+del model
--- witschey/searchers/__init__.py
+++ witschey/searchers/__init__.py
@@ -1,2 +1,4 @@
-from simulated_annealer import SimulatedAnnealer
-from maxwalksat import MaxWalkSat
+from simulated_annealer import SimulatedAnnealer ; del simulated_annealer
+from maxwalksat import MaxWalkSat ; del maxwalksat
+
+del searcher
