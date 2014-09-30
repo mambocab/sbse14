691c7cd794f8c35ea85751cdc0684e57c09d26df
Jim Witschey <jim.witschey@gmail.com>
2014-09-30 13:21:06 -0400

    some adaptations -- not done!

--- witschey/searchers/GeneticAlgorithm.py
+++ witschey/searchers/GeneticAlgorithm.py
@@ -1,75 +1,57 @@
 from __future__ import division
 
 #Structure from SA Lecture
-import sys, re, random, math
-import copy
+import sys, re, random, math, itertools
 
-from options import *
-from utils import *
-from analyzer import *
-
-myOpt = Options()
+from searcher import Searcher
 
 # adapted from Chris Theisen's code
 #     his code provided the shell that I worked in and styled to my liking
 #Structure from:
 #http://www.cleveralgorithms.com/nature-inspired/evolution/genetic_algorithm.html
-class GA:
+class GeneticAlgorithm(Searcher):
+
+    def __init__(self, model *args, **kw):
+        super(GA, self).__init__(model=model, *args, **kw)
 
     def mutate(self, child, pMut):
         #nothing happens! Hooray! TODO
         return child
 
-  def Crossover(self, parent1, parent2, crossover):
-    #BECAUSE REASONS OK TODO
-    return parent1, parent2
+    def crossover(self, parent1, parent2, crossover):
+        #BECAUSE REASONS OK TODO
+        return parent1, parent2
     
-  def SelectParents(self, pop): #all possible parents
-    temp = []
-    for x in pop:
-      for y in pop:
-        if x != y:
-          temp.append(x, y)
-    return temp
-          
-  def GetBestSolution(self, pop):
-    eMin = 1
-    for x in pop:
-      temp = x.Energy
-      if temp < 1:
-        eMin = temp
-    return eMin
+    def select_parents(self, pop): #all possible parents
+        a = itertools.combinations(pop, 2)
+        b = itertools.combinations(reversed(pop), 2)
+        return set(a).union(set(b))
 
-  def run(self, klass):
-    ga = klass
-    pMutate = 1/len(ga.XVar)
-    XVarBest = ga.XVar
-    #print 'start energy: ', eBest           
-    k = 1
-    eList = []
-    population = []
-    analyze = Analyzer()
-    stop = False
-    
-    for i in range(myOpt.ga_pop_size):
-      population.append(copy.deepcopy(ga).Chaos()) #add a randomly generated model to list
-  
-    for gens in myOpt.ga_gen_list:
-      eBest = 1
-      while k < gens:
-        parents = self.SelectParents(population) #all possible pairings of parents
-        children = []
-        for parent1, parent2 in parents:
-          child1, child2 = self.Crossover(parent1, parent2, myOpt.ga_crossover)
-          children.append(self.Mutate(child1, pMutate))
-          children.append(self.Mutate(child2, pMutate))
-        eBest = self.GetBestSolution(children)
-        population = children
-        k += 1
-      eList.append(eBest)
-      #some "is significantly better" termination logic here
-    
-    return min(eList), True
+    def run(self, text_report=True):
+        p_mutation = 1 / len(self.model.xs)
+        riv = self.model.random_input_vector
+        XVarBest = riv()
+        eList = []
+        population = [riv() for _ in self.spec.population_size]
+        stop = False
+
+        k = 1
+        for gens in myOpt.ga_gen_list:
+            eBest = 1
+            while k < gens:
+                parents = self.select_parents(population)
+                children = []
+                for parent1, parent2 in parents:
+                    child1, child2 = self.Crossover(parent1, parent2, myOpt.  ga_crossover  )
+                    children.append(self.Mutate(child1, pMutate))
+                    children.append(self.Mutate(child2, pMutate))
+                eBest = min([c.energy for c in children])
+                population = children
+                k += 1
+            eList.append(eBest)
+            #some "is significantly better" termination logic here
+        
+        return min(eList), True
 
   def printOptions(self):
     print "GA Options:"
