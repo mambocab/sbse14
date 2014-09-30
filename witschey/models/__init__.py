ad3749d8cc38b9d8dc084712c4467a921ecadce7
Jim Witschey <jim.witschey@gmail.com>
2014-09-30 13:52:46 -0400

    adds dltz7 to models

--- witschey/models/__init__.py
+++ witschey/models/__init__.py
@@ -5,5 +5,6 @@ from fonseca import Fonseca; del fonseca
 from zdt1 import ZDT1; del zdt1
 from zdt3 import ZDT3; del zdt3
 from viennet3 import Viennet3; del viennet3
+from dtlz7 import DTLZ7; del dtlz7
 
 del model
