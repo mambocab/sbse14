1dffc34db9c991d43e8749b3b509ca5630b400c8
Jim Witschey <jim.witschey@gmail.com>
2014-10-07 13:12:19 -0400

    performance testing

--- perf/pairs/speedtest.py
+++ perf/pairs/speedtest.py
@@ -2,10 +2,6 @@ from __future__ import print_function
 
 import itertools
 
-def pairs_2(xs):
-    for p in itertools.izip(xs[:-1], xs[1:]): 
-        yield p
-
 def pairs_0(lst):
     last = object()
     dummy = last
@@ -18,11 +14,27 @@ def pairs_1(xs):
     for ii in range(1,len(xs)):
         yield xs[ii-1], xs[ii]
 
-d = {2: pairs_2, 0: pairs_0, 1: pairs_1}
+def pairs_2(xs):
+    for p in itertools.izip(xs[:-1], xs[1:]): 
+        yield p
+
+def pairs_3(xs):
+    a, b = itertools.tee(xs)
+    next(b, None)
+    for p in itertools.izip(a, b):
+        yield p
+
+d = {
+    0: pairs_0,
+    1: pairs_1,
+    2: pairs_2,
+    3: pairs_3,
+}
 
 def test(i, n, w):
     r = range(2 ** n)
     if w is list:
         r = list(r)
     big_list = list(d[i](r))
+
 
--- perf/pairs/speedtest.sh
+++ perf/pairs/speedtest.sh
@@ -6,6 +6,8 @@ echo -n '0 slices: '
 python -m timeit -s'import speedtest' 'speedtest.test(0, 10, None)'
 echo -n "Lenski's: "
 python -m timeit -s'import speedtest' 'speedtest.test(1, 10, None)'
+echo -n "recipe:   "
+python -m timeit -s'import speedtest' 'speedtest.test(3, 10, None)'
 echo
 
 echo '2 ** 15 items'
@@ -15,3 +17,6 @@ echo -n '0 slices: '
 python -m timeit -s'import speedtest' 'speedtest.test(0, 15, None)'
 echo -n "Lenski's: "
 python -m timeit -s'import speedtest' 'speedtest.test(1, 15, None)'
+echo -n "recipe:   "
+python -m timeit -s'import speedtest' 'speedtest.test(3, 15, None)'
+echo
