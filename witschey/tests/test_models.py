c61f1fbad7d9297b43e0b7a286666a11b9ad9654
Jim Witschey <jim.witschey@gmail.com>
2014-10-07 13:21:26 -0400

    unbreaks tests for schaffer

--- witschey/tests/test_models.py
+++ witschey/tests/test_models.py
@@ -1,9 +1,15 @@
+from __future__ import division
+
 from witschey import models
 from unittest import TestCase
 
 class TestSchaffer(TestCase):
     def setUp(self):
         self.model = models.Schaffer()
+
+    def test_different_dependents(self):
+        names = [y.__name__ for y in self.model.ys]
+        assert len(names) == len(self.model.ys)
 
     def test_inputs_length(self):
         i = 10
@@ -16,11 +22,13 @@ class TestSchaffer(TestCase):
         assert x == 1, 'len(model.xs) == {}'.format(x)
 
     def test_f1(self):
-        assert self.model.f1((0,)) == 0
-        assert self.model.f1([2]) == 4
-        assert self.model.f1((-2, 2)) == 8
+        f1 = next(y for y in self.model.ys if y.__name__ == 'f1')
+        assert f1((0,)) == 0
+        assert f1([2]) == 4
+        assert f1((-2, 2)) == 8
 
     def test_f2(self):
-        assert self.model.f2((0,)) == 4
-        assert self.model.f2([2]) == 0
-        assert self.model.f2((0, 0)) == 8
+        f2 = next(y for y in self.model.ys if y.__name__ == 'f2')
+        assert f2((0,)) == 4
+        assert f2([2]) == 0
+        assert f2((0, 0)) == 8
