637380af0e4d141505b3035f9ae4cee4ff4fa69b
Jim Witschey <jim.witschey@gmail.com>
2014-10-08 14:29:43 -0400

    cleans up models __init__.py

--- witschey/models/__init__.py
+++ witschey/models/__init__.py
@@ -1,11 +1,11 @@
 from model import Model
-from independent_variable import IndependentVariable; del independent_variable
-from schaffer import Schaffer; del schaffer
-from kursawe import Kursawe; del kursawe
-from fonseca import Fonseca; del fonseca
-from zdt1 import ZDT1; del zdt1
-from zdt3 import ZDT3; del zdt3
-from viennet3 import Viennet3; del viennet3
-from dtlz7 import DTLZ7; del dtlz7
+from independent_variable import IndependentVariable
+from schaffer import Schaffer
+from kursawe import Kursawe
+from fonseca import Fonseca
+from zdt1 import ZDT1
+from zdt3 import ZDT3
+from viennet3 import Viennet3
+from dtlz7 import DTLZ7
 
 del model
