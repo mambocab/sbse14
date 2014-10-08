0bff709276a5dcf1a5885c6fb1dbe11ae95ab3bb
Jim Witschey <jim.witschey@gmail.com>
2014-10-08 17:30:27 -0400

    some experimentation and cleanup with GA

--- witschey/searchers/genetic_algorithm.py
+++ witschey/searchers/genetic_algorithm.py
@@ -3,7 +3,7 @@ from __future__ import division, print_function
 import itertools, random
 
 from witschey import base
-from searcher import Searcher
+from searcher import Searcher, SearchIO
 
 # adapted from Chris Theisen's code
 #     his code provided the shell that I worked in and styled to my liking
@@ -25,7 +25,7 @@ class GeneticAlgorithm(Searcher):
         if len(parent1) != len(parent2):
             raise ValueError('parents must be same length to breed')
 
-        x_pts = sorted(random.sample(range(1, len(parent1) - 1), crossovers))
+        x_pts = sorted(random.sample(xrange(1, len(parent1) - 1), crossovers))
         x_pts = itertools.chain((0,), x_pts, (None,))
 
         ugh_mom_dad = itertools.cycle((parent1, parent2))
@@ -35,37 +35,34 @@ class GeneticAlgorithm(Searcher):
 
         return tuple(itertools.chain(*segments))
     
-    def select_parents(self, population): #all possible parents
+    def select_parents(self, population, output_size): #all possible parents
         xs = itertools.combinations(population, 2)
         ys = itertools.combinations(reversed(population), 2)
-        return random.sample(set(a).union(set(b)), self.spec.population_size)
+        return random.sample(set(a).union(set(b)), output_size)
 
     def run(self, text_report=True):
-        p_mutation = 1 / len(self.model.xs)
-        rand_vect = self.model.random_input_vector
-        population = [rand_vect() for _ in xrange(self.spec.population_size)]
-        eList = []
+        rand_vect = lambda: self.model.random_input_vector()
+        max_pop = self.spec.population_size
+        init_xs = (rand_vect() for _ in xrange(max_pop))
+        import pdb; pdb.set_trace()
+        init_xs_ys = itertools.izip(init_xs, map(self.model, init_xs))
+        population = map(lambda x, y: SearchIO(y, y, self.model.energy(y)),
+            init_xs_ys)
+
         stop = False
 
-        state = {
-            'best':    {'xs': None, 'ys': None, 'energy': None},
-            'current': {'xs': None, 'ys': None, 'energy': None}
-            }
+        best = min(population, key=lambda x: x.energy)
 
-        k = 1
-        for gens in myOpt.ga_gen_list:
-            eBest = 1
-            while k < gens:
-                parent_pairs = self.select_parents(population)
-                children = []
-                for parent1, parent2 in parent_pairs:
-                    child = self.crossover(parent1, parent2)
-                    children.append(self.mutate(child1, p_mutation))
-                    children.append(self.mutate(child2, p_mutation))
-                eBest = min([c.energy for c in children])
-                population = children
-                k += 1
-            eList.append(eBest)
+        for gens in xrange(self.spec.iterations):
+            children = []
+            for parent1, parent2 in self.select_parents(population, max_pop):
+                child = self.crossover(parent1, parent2)
+                children.append(self.mutate(child2, p_mutation))
+            best_in_pop = min(c.energy for c in children)
+            if best_in_pop.energy < best.energy:
+                best = best_in_pop
+
+            population = children
             #some "is significantly better" termination logic here
 
-        return min(eList), True
+        return best.energy
