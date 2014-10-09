d883851fa206249a6420dab524f8ef8b0d036706
Jim Witschey <jim.witschey@gmail.com>
2014-10-09 13:05:55 -0400

    more intermediate hw5 stuff

--- hw5/search.py
+++ hw5/search.py
@@ -1,11 +1,25 @@
 from __future__ import print_function, division
 
+import itertools
+
 from witschey.models import Model
 from witschey.searchers import Searcher, SearcherConfig
 
+from witschey.searchers import *
+from witschey.models import *
+
+import timeit
+
 def run():
-    for m in Model.__subclasses__():
-        print(m.__name__)
+    ss, ms = Searcher.__subclasses__(), (Fonseca,)
+    # ss, ms = Searcher.__subclasses__(), Model.__subclasses__()
+    for searcher, model in itertools.product(ss, ms):
+        print('{}({})'.format(searcher.__name__, model.__name__))
+        searcher = searcher(model)
+        out = searcher.run()
+        print('{}:\n{}'.format(model.__name__, out.to_str()))
+        print('#' * 40)
+        print()
 
 if __name__ == '__main__':
     run()
