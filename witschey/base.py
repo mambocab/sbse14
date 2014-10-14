78b98f3c0cf303e60f1668a10e7b281d01bf78db
Jim Witschey <jim.witschey@gmail.com>
2014-10-14 18:36:03 -0400

    more global options

--- witschey/base.py
+++ witschey/base.py
@@ -121,7 +121,9 @@ class NullObject(object):
 The = memo(
     Searcher=memo(era_length=50, terminate_early=True,
         log_eras_best_energy=True, log_eras_by_objective=False,
-        iterations=1000, p_mutation=1/3),
+        iterations=1000, p_mutation=1/3, epsilon=.01),
     SimulatedAnnealer=memo(cooling_factor=.8),
     MaxWalkSat=memo(),
-    GeneticAlgorithm=memo(population_size=50, p_mutation=.6))
+    GeneticAlgorithm=memo(population_size=50, p_mutation=.6),
+    DifferentialEvolution=memo(generations=100, n_candiates=100,
+        f=.75, p_crossover=.3))
