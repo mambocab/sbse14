5881b25020d4ac5577ab082dff71f722fc5d2bfc
Jim Witschey <jim.witschey@gmail.com>
2014-10-14 11:49:13 -0400

    provides searcher shortnames dict

--- witschey/searchers/__init__.py
+++ witschey/searchers/__init__.py
@@ -2,3 +2,12 @@ from searcher import Searcher, SearcherConfig
 from simulated_annealer import SimulatedAnnealer
 from maxwalksat import MaxWalkSat
 from genetic_algorithm import GeneticAlgorithm
+
+SEARCHER_SHORTNAMES = {
+    SimulatedAnnealer: 'SA',
+    MaxWalkSat: 'MWS',
+    GeneticAlgorithm: 'GA'
+}
+SEARCHER_SHORTNAMES.update(
+    {cls.__name__: shortname
+    for cls, shortname in SEARCHER_SHORTNAMES.iteritems()})
