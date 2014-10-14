2bf286c7d004e86676d7666980a0ade26badaaf5
Jim Witschey <jim.witschey@gmail.com>
2014-10-14 17:35:21 -0400

    adds differential evolution

--- witschey/searchers/__init__.py
+++ witschey/searchers/__init__.py
@@ -2,11 +2,13 @@ from searcher import Searcher, SearcherConfig
 from simulated_annealer import SimulatedAnnealer
 from maxwalksat import MaxWalkSat
 from genetic_algorithm import GeneticAlgorithm
+from differential_evolution import DifferentialEvolution
 
 SEARCHER_SHORTNAMES = {
-    SimulatedAnnealer: 'SA',
-    MaxWalkSat: 'MWS',
-    GeneticAlgorithm: 'GA'
+    SimulatedAnnealer:     'SA',
+    MaxWalkSat:            'MWS',
+    GeneticAlgorithm:      'GA',
+    DifferentialEvolution: 'DE'
 }
 SEARCHER_SHORTNAMES.update(
     {cls.__name__: shortname
---
+++ witschey/searchers/differential_evolution.py
@@ -0,0 +1,11 @@
+from __future__ import division, print_function
+
+class DifferentialEvolution(Searcher):
+
+    def __init__(self, model, *args, **kw):
+        super(GeneticAlgorithm, self).__init__(model=model, *args, **kw)
+
+    def run(self, text_report=True):
+        pass
+
+
