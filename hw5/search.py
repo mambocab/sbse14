4c84eea7e3cc347664e0be2def5ca1e085764952
Jim Witschey <jim.witschey@gmail.com>
2014-10-14 11:50:17 -0400

    adds rdivdemo to searching

--- hw5/search.py
+++ hw5/search.py
@@ -1,27 +1,40 @@
 from __future__ import print_function, division
 
-import itertools
+import itertools, random
+from collections import namedtuple, defaultdict
 
 from witschey.models import Model
 from witschey.searchers import Searcher, SearcherConfig
+from witschey.searchers import SEARCHER_SHORTNAMES as SSNAMES
 
-from witschey.searchers import *
-from witschey.models import *
+from witschey.rdiv import rdivDemo
 
 import timeit
 
-def run():
-    ss, ms = Searcher.__subclasses__(), (Fonseca,)
-    # ss, ms = Searcher.__subclasses__(), Model.__subclasses__()
-    for searcher, model in itertools.product(ss, ms):
-        print('{}({})'.format(searcher.__name__, model.__name__))
-        searcher = searcher(model)
-        out = searcher.run()
-        print('{}:\n{}'.format(model.__name__, out.to_str()))
-        print('#' * 40)
-        print()
+def run(n=30, text_report=True):
+    ss, ms = Searcher.__subclasses__(), Model.__subclasses__()
+    random.seed(1)
+    outs = []
+    last_logs = defaultdict(list)
+    for model_cls in ms:
+        print("#### {}".format(model_cls.__name__))
+        bests = defaultdict(list)
+        for searcher_cls in ss:
+            for _ in xrange(n):
+                Output = namedtuple('Output', ('name', 'best'))
+                name = ('{}'.format(SSNAMES[searcher_cls]))
+                searcher = searcher_cls(model_cls)
+                out = searcher.run(text_report=text_report)
+                out.searcher = searcher_cls
+                outs.append(out)
+                last_index = max(out.era_logs_best_energy.keys())
+                last_logs[name].append(out.era_logs_best_energy[last_index])
+                bests[name].append(out.best)
+
+        rdiv_in = list([name] + log.contents()
+            for name, log_list in last_logs.iteritems()
+            for log in log_list)
+        print(rdivDemo(rdiv_in))
 
 if __name__ == '__main__':
-    run()
-    sc = SearcherConfig()
-    print(sc)
+    run(n = 5, text_report=False)
