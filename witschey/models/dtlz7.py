408f2ddf4606c61039fe2974532f5290b4e2e6c5
Jim Witschey <jim.witschey@gmail.com>
2014-10-15 16:53:27 -0400

    flake8 fixes in models/dtlz7.py

--- witschey/models/dtlz7.py
+++ witschey/models/dtlz7.py
@@ -5,7 +5,8 @@ from __future__ import division
 import math
 
 from model import Model
-from independent_variable import IndependentVariable as IV
+from independent_variable import IndependentVariable as IV  # noqa
+
 
 class DTLZ7(Model):
     def __init__(self, ivs=30, dvs=20):
