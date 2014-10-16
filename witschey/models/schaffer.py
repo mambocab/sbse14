242e062339eeafca6b4fb8f15129d7518e3525b5
Jim Witschey <jim.witschey@gmail.com>
2014-10-15 20:22:19 -0400

    flake8 fixes for schaffer

--- witschey/models/schaffer.py
+++ witschey/models/schaffer.py
@@ -2,11 +2,12 @@
 # https://github.com/timm/sbse14/blob/master/models.py
 
 from model import Model
-from independent_variable import IndependentVariable as IV
+from independent_variable import IndependentVariable as IV  # noqa
+
 
 class Schaffer(Model):
     def __init__(self, ivs=1):
-        ivs = tuple(IV(lo=-10^5, hi=10^5) for _ in xrange(ivs))
+        ivs = tuple(IV(lo=-10 ** 5, hi=10 ** 5) for _ in xrange(ivs))
         # we use def instead of lambdas so the functions keep their __name__s
         def f1(xs):
             return sum(x ** 2 for x in xs)
