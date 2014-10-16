140593f6e9b5cce09118776a820fa0a71a360e59
Jim Witschey <jim.witschey@gmail.com>
2014-10-15 22:33:43 -0400

    tests maxwalksat

--- witschey/tests/test_searchers.py
+++ witschey/tests/test_searchers.py
@@ -2,6 +2,7 @@ from __future__ import division
 
 import random
 from nose.tools import assert_equals, assert_is_instance, assert_is_none
+from nose.tools import assert_greater_equal, assert_less_equal
 from nose.tools import assert_true
 
 from witschey import searchers
@@ -71,4 +72,31 @@ class TestSearcherConfig(TestCase):
         assert_is_instance(searcher.model, m)
         assert_is_instance(searcher, s)
 
+
+class TestMaxWalkSat(TestCase):
+
+    def setUp(self):
+        self.mws = searchers.MaxWalkSat(models.Schaffer)
+
+    def test_init(self):
+        s = searchers.MaxWalkSat(models.Schaffer, iterations=222)
+        assert_equals(s.spec.iterations, 222)
+
+    def test_local_inputs_length(self):
+        n = 15
+        assert_equals(n, len(list(self.mws.local_search_inputs(3, 6, n))))
+
+    def test_local_inputs_contents(self):
+        xs = self.mws.local_search_inputs(0, 20, 20)
+        random.seed(1)
+        # this is stochastic, so run it 100 times & hope any errors are caught
+        for _ in xrange(100):
+            for i, x in enumerate(xs):
+                assert_greater_equal(i + 1, x)
+                assert_less_equal(i, x)
+
+    def test_doesnt_fall_over(self):
+        """this test doesn't help much, except to make sure there aren't any 
+        dumb type errors"""
+        self.mws = searchers.MaxWalkSat(models.Schaffer, iterations=300)
 
