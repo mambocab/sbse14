1413a4a4402578bfe950f709588070ba76a71503
Jim Witschey <jim.witschey@gmail.com>
2014-10-15 22:30:39 -0400

    switch test_base to nose.tool assertions

--- witschey/tests/test_base.py
+++ witschey/tests/test_base.py
@@ -4,40 +4,47 @@ from witschey import base
 from unittest import TestCase
 
 import types
+from nose.tools import assert_equal, assert_true
+import mock
+import math
 
 class TestPairs(TestCase):
+
     def test_type(self):
-        assert isinstance(base.pairs([1,2]), collections.Iterable)
+        assert_true(isinstance(base.pairs([1,2]), collections.Iterable))
 
     def test_empty(self):
-        assert list(base.pairs([])) == []
+        assert_equal(list(base.pairs([])), [])
 
     def test_tuple(self):
-        assert list(base.pairs((1, 2, 3))) == [(1, 2), (2, 3)]
+        assert_equal(list(base.pairs((1, 2, 3))), [(1, 2), (2, 3)])
 
     def test_one_element(self):
-        assert list(base.pairs([1])) == []
+        assert_equal(list(base.pairs([1])), [])
 
     def test_many_elements(self):
         ps = list(base.pairs([x for x in xrange(15)]))
-        assert len(ps) == 14, len(ps)
+        assert_equal(len(ps), 14)
         for p in ps:
-            assert p[0] == p[1] - 1
+            assert_equal(p[0], p[1] - 1)
+
 
 class TestMemo(TestCase):
+
     def test_basic(self):
         v = 1
         m = base.memo(a=v)
-        assert m.a == v
+        assert_equal(m.a, v)
 
     def test_nested(self):
         v = 1
         m = base.memo(a=base.memo(a=v))
-        assert m.a.a == v
+        assert_equal(m.a.a, v)
 
     def test_multiple(self):
         v1 = 't'
         v2 = 3
         m = base.memo(a=v1, b= v2)
-        assert m.a == v1
-        assert m.b == v2
+        assert_equal(m.a, v1)
+        assert_equal(m.b, v2)
+
