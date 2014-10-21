ef2c3b0a94c0c3af38e45777146d93994c8537e5
Jim Witschey <jim.witschey@gmail.com>
2014-10-21 03:06:10 -0400

    removes unnecessary stuff from rdiv

--- witschey/rdiv.py
+++ witschey/rdiv.py
@@ -1,59 +1,19 @@
-"""
-
-## Hyptotheis Testing Stuff
-
-
-### Standard Stuff
-
-#### Standard Headers
-
-"""
 from __future__ import division
-import sys,random,math
-sys.dont_write_bytecode = True
+import sys
+import random
+import math
+
+import texttable
+from basic_stats import xtile, median
+from witschey import base
 
 # flake8: noqa
 
-import texttable
-
-"""
-
-#### Standard Utils
-
-"""
-class o():
-  "Anonymous container"
-  def __init__(i,**fields) : 
-    i.override(fields)
-  def override(i,d): i.__dict__.update(d); return i
-  def __repr__(i):
-    d = i.__dict__
-    name = i.__class__.__name__
-    return name+'{'+' '.join([':%s %s' % (k,pretty(d[k])) 
-                     for k in i.show()])+ '}'
-  def show(i):
-    return [k for k in sorted(i.__dict__.keys()) 
-            if not "_" in k]
-"""
-
-Misc functions:
-
-"""
 rand = random.random
-any  = random.choice
 seed = random.seed
 exp  = lambda n: math.e**n
 ln   = lambda n: math.log(n,math.e)
 g    = lambda n: round(n,2)
-
-def median(lst,ordered=False):
-  if not ordered: lst= sorted(lst)
-  n = len(lst)
-  p = n//2
-  if n % 2: return lst[p]
-  q = p - 1
-  q = max(0,min(q,n))
-  return (lst[p] + lst[q])/2
 
 def msecs(f):
   import time
@@ -67,41 +27,6 @@ def pairs(lst):
   for i in lst[1:]:
     yield last,i
     last = i
-
-def xtile(lst,lo=0,hi=100,width=50,
-             chops=[0.1 ,0.3,0.5,0.7,0.9],
-             marks=["-" ," "," ","-"," "],
-             bar="|",star="*",show=" %3.0f",
-             as_list=False):
-  """The function _xtile_ takes a list of (possibly)
-  unsorted numbers and presents them as a horizontal
-  xtile chart (in ascii format). The default is a 
-  contracted _quintile_ that shows the 
-  10,30,50,70,90 breaks in the data (but this can be 
-  changed- see the optional flags of the function).
-  """
-  def pos(p)   : return ordered[int(len(lst)*p)]
-  def place(x) : 
-    return int(width*float((x - lo))/(hi - lo+0.00001))
-  def pretty(lst) : 
-    return ', '.join([show % x for x in lst])
-  ordered = sorted(lst)
-  lo      = min(lo,ordered[0])
-  hi      = max(hi,ordered[-1])
-  what    = [pos(p)   for p in chops]
-  where   = [place(n) for n in  what]
-  out     = [" "] * width
-  for one,two in pairs(where):
-    for i in range(one,two): 
-      out[i] = marks[0]
-    marks = marks[1:]
-  out[int(width/2)]    = bar
-  out[place(pos(0.5))] = star 
-  if as_list:
-    rv = ['(' + ''.join(out) + ")"]
-    rv.extend(show % x for x in what)
-    return rv
-  return '('+''.join(out) +  ")," +  pretty(what)
 
 def _tileX() :
   import random
@@ -198,8 +123,8 @@ def a12(lst1,lst2):
   lst2 = sorted(lst2, reverse=True)
   n1   = len(lst1)
   n2   = len(lst2)
-  t1   = o(l=lst1,j=0,eq=0,gt=0,n=n1)
-  t2   = o(l=lst2,j=0,eq=0,gt=0,n=n2)
+  t1   = base.memo(l=lst1,j=0,eq=0,gt=0,n=n1)
+  t2   = base.memo(l=lst2,j=0,eq=0,gt=0,n=n2)
   gt,eq= loop(t1, t1, t2)
   return gt/(n1*n2) + eq/2/(n1*n2)
 
@@ -509,13 +434,3 @@ def rdiv_report(data):
   table.set_deco(texttable.Texttable.HEADER)
   table.add_rows(rows)
   return table.draw()
-
-def rdiv8():
-    print(rdivDemo([
-        ['TPBs', 208, 176, 321, 128, 128],
-        ['phil', 688, 346, 290, 524],
-        ["'zines", 28, 76, 32, 64],
-        ['comp', 398, 312, 361, 436, 316]
-    ]))
-
-if __name__ == "__main__": rdiv8()
