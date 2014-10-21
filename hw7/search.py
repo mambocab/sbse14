6d8121a31061ee1288bdd03ce5c82b86023a85df
Jim Witschey <jim.witschey@gmail.com>
2014-10-21 14:24:38 -0400

    inits hw7

---
+++ hw7/search.py
@@ -0,0 +1,39 @@
+from __future__ import print_function, division
+
+import itertools, random
+from collections import namedtuple, defaultdict
+
+from witschey.models import Model, Schwefel
+from witschey.searchers import Searcher
+
+from witschey.rdiv import rdiv_report
+
+import timeit
+
+def run(n=30, text_report=True):
+    ms = Model.__subclasses__()
+    ms.remove(Schwefel)
+    ms.extend(Schwefel.initalizer_with(d=d) for d in (10, 20, 40))
+    ss = Searcher.__subclasses__()
+    random.seed(1)
+    outs = []
+    last_logs = defaultdict(list)
+    for model_cls in ms:
+        print("#### {}".format(model_cls.__name__))
+        bests = defaultdict(list)
+        for searcher_cls in ss:
+            for _ in xrange(n):
+                Output = namedtuple('Output', ('name', 'best'))
+                name = ('{}'.format(searcher_cls.shortname))
+                searcher = searcher_cls(model_cls)
+                out = searcher.run(text_report=text_report)
+                if not hasattr(out, 'searcher'):
+                    out.searcher = searcher_cls
+                outs.append(out)
+                bests[name].extend(out.best_era.contents())
+
+        rdiv_in = list([name] + best for name, best in bests.iteritems())
+        print(rdiv_report(rdiv_in), end='\n\n')
+
+if __name__ == '__main__':
+    run(n=2, text_report=True)
