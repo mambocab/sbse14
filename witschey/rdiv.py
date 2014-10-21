84b2eed58629dcc08dee11f3e03742ca2e806f03
Jim Witschey <jim.witschey@gmail.com>
2014-10-21 03:20:29 -0400

    removes unused function

--- witschey/rdiv.py
+++ witschey/rdiv.py
@@ -61,21 +61,6 @@ class Num:
       return i.all[int(n2)] - i.all[int(n1)]
 
 
-"""
-
-### The A12 Effect Size Test 
-
-"""
-def a12slow(lst1,lst2):
-  "how often is x in lst1 more than y in lst2?"
-  more = same = 0.0
-  for x in lst1:
-    for y in lst2:
-      if    x == y : same += 1
-      elif  x >  y : more += 1
-  x= (more + 0.5*same) / (len(lst1)*len(lst2))
-  return x
-
 def a12(lst1,lst2):
   "how often is x in lst1 more than y in lst2?"
   def loop(t,t1,t2): 
