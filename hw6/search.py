b730cff161c7dacf83e660dc2abb15b82ddfb6da
Jim Witschey <jim.witschey@gmail.com>
2014-10-21 01:56:01 -0400

    adds new Schwefels to search

--- hw6/search.py
+++ hw6/search.py
@@ -3,19 +3,18 @@ from __future__ import print_function, division
 import itertools, random
 from collections import namedtuple, defaultdict
 
-from witschey.models import Model, Schwefel, Viennet3, Schaffer
-from witschey.searchers import Searcher, SearcherConfig, DifferentialEvolution
-from witschey.searchers import SEARCHER_SHORTNAMES as SSNAMES
+from witschey.models import Model, Schwefel
+from witschey.searchers import Searcher
 
 from witschey.rdiv import rdivDemo
 
 import timeit
 
 def run(n=30, text_report=True):
-    ss, ms = Searcher.__subclasses__(), Model.__subclasses__()
-    # ss, ms = Searcher.__subclasses__(), (Schaffer,)
-    # ss, ms = (DifferentialEvolution,), Model.__subclasses__()
-    # ss, ms = (DifferentialEvolution,), (Viennet3,)
+    ms = Model.__subclasses__()
+    ms.remove(Schwefel)
+    ms.extend(Schwefel.initalizer_with(d=d) for d in (10, 20, 40))
+    ss = Searcher.__subclasses__()
     random.seed(1)
     outs = []
     last_logs = defaultdict(list)
@@ -28,12 +27,13 @@ def run(n=30, text_report=True):
                 name = ('{}'.format(searcher_cls.shortname))
                 searcher = searcher_cls(model_cls)
                 out = searcher.run(text_report=text_report)
-                out.searcher = searcher_cls
+                if not hasattr(out, 'searcher'):
+                    out.searcher = searcher_cls
                 outs.append(out)
                 bests[name].append(out.best)
 
         rdiv_in = list([name] + best for name, best in bests.iteritems())
-        print(rdivDemo(rdiv_in))
+        print(rdivDemo(rdiv_in), end='\n\n')
 
 if __name__ == '__main__':
     run(n = 2, text_report=False)
