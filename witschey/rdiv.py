b6053b66f700373f86e9db79a56980832c2beaee
Jim Witschey <jim.witschey@gmail.com>
2014-10-21 12:50:59 -0400

    rdiv indentation

--- witschey/rdiv.py
+++ witschey/rdiv.py
@@ -62,30 +62,32 @@ class Num:
 
 
 def a12(lst1,lst2):
-  "how often is x in lst1 more than y in lst2?"
-  def loop(t,t1,t2): 
-    while t1.j < t1.n and t2.j < t2.n:
-      h1 = t1.l[t1.j]
-      h2 = t2.l[t2.j]
-      h3 = t2.l[t2.j+1] if t2.j+1 < t2.n else None 
-      if h1>  h2:
-        t1.j  += 1; t1.gt += t2.n - t2.j
-      elif h1 == h2:
-        if h3 and h1 > h3 :
-            t1.gt += t2.n - t2.j  - 1
-        t1.j  += 1; t1.eq += 1; t2.eq += 1
-      else:
-        t2,t1  = t1,t2
-    return t.gt*1.0, t.eq*1.0
-  #--------------------------
-  lst1 = sorted(lst1, reverse=True)
-  lst2 = sorted(lst2, reverse=True)
-  n1   = len(lst1)
-  n2   = len(lst2)
-  t1   = base.memo(l=lst1,j=0,eq=0,gt=0,n=n1)
-  t2   = base.memo(l=lst2,j=0,eq=0,gt=0,n=n2)
-  gt,eq= loop(t1, t1, t2)
-  return gt/(n1*n2) + eq/2/(n1*n2)
+    "how often is x in lst1 more than y in lst2?"
+    def loop(t,t1,t2): 
+        while t1.j < t1.n and t2.j < t2.n:
+            h1 = t1.l[t1.j]
+            h2 = t2.l[t2.j]
+            h3 = t2.l[t2.j+1] if t2.j+1 < t2.n else None 
+            if h1>  h2:
+                t1.j  += 1; t1.gt += t2.n - t2.j
+            elif h1 == h2:
+                if h3 and h1 > h3 :
+                    t1.gt += t2.n - t2.j  - 1
+                t1.j  += 1; t1.eq += 1; t2.eq += 1
+            else:
+                t2,t1  = t1,t2
+        return t.gt*1.0, t.eq*1.0
+    #--------------------------
+    lst1 = sorted(lst1, reverse=True)
+    lst2 = sorted(lst2, reverse=True)
+    n1   = len(lst1)
+    n2   = len(lst2)
+    t1   = base.memo(l=lst1,j=0,eq=0,gt=0,n=n1)
+    t2   = base.memo(l=lst2,j=0,eq=0,gt=0,n=n2)
+    gt,eq= loop(t1, t1, t2)
+    return gt/(n1*n2) + eq/2/(n1*n2)
+
+
 """## Non-Parametric Hypothesis Testing
 
 The following _bootstrap_ method was introduced in
