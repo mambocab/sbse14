40da6e2681dd4937e2191d26afd3684b476b4ac3
Jim Witschey <jim.witschey@gmail.com>
2014-10-24 18:47:22 -0400

    moves test_searchers to its own dir

--- witschey/tests/test_searchers/test_searchers.py
+++ witschey/tests/test_searchers/test_searchers.py
@@ -9,31 +9,6 @@ from unittest import TestCase
 from witschey import models
 
 
-class TestGeneticAlgorithm(TestCase):
-    def setUp(self):  # noqa
-        self.model = searchers.GeneticAlgorithm(models.Schaffer)
-
-    def test_crossover_lengths(self):
-        parent1 = tuple(i * 10 for i in range(10))
-        parent2 = tuple((i * 10) + 1 for i in range(10))
-
-        xo = self.model.crossover(parent1, parent2, 3)
-
-        assert len(parent1) == len(xo)
-        assert len(xo) == len(parent2)
-
-    def test_crossover_values(self):
-        random.seed(0)
-        parent1 = tuple(i * 10 for i in range(10))
-        parent2 = tuple((i * 10) + 1 for i in range(10))
-
-        xo = self.model.crossover(parent1, parent2, 3)
-
-        for i, x in enumerate(xo):
-            print(x, parent1[1], parent2[i])
-            assert x == parent1[i] or x == parent2[i]
-
-
 class TestSearcherConfig(TestCase):
 
     def test_empty_config(self):
