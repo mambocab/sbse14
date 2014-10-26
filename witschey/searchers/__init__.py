f06094aafa56fc179ce3f241f956832c88bb1dbc
Jim Witschey <jim.witschey@gmail.com>
2014-10-25 21:04:14 -0400

    inits PSO

--- witschey/searchers/__init__.py
+++ witschey/searchers/__init__.py
@@ -3,6 +3,7 @@ from simulated_annealer import SimulatedAnnealer
 from maxwalksat import MaxWalkSat
 from genetic_algorithm import GeneticAlgorithm
 from differential_evolution import DifferentialEvolution
+from particle_swarm_optimizer import ParticleSwarmOptimizer
 
 SEARCHER_SHORTNAMES = {
     SimulatedAnnealer:     'SA',
@@ -16,4 +17,4 @@ for searcher_cls, shortname in SEARCHER_SHORTNAMES.iteritems():
 
 __all__ = [Searcher, SearcherConfig, SearchReport,
            SimulatedAnnealer, MaxWalkSat,
-           GeneticAlgorithm, DifferentialEvolution]
+           GeneticAlgorithm, DifferentialEvolution, ParticleSwarmOptimizer]
---
+++ witschey/searchers/particle_swarm_optimizer.py
@@ -0,0 +1,14 @@
+from __future__ import division, print_function
+
+from searcher import Searcher
+
+
+class ParticleSwarmOptimizer(Searcher):
+    """
+    A searcher that models a "flock" of individuals roaming the search space.
+    Individuals make decisions about where to go next based both on their
+    own experience and on the experience of the whole group. For more
+    information, see https://github.com/timm/sbse14/wiki/pso#details and
+    http://en.wikipedia.org/wiki/Particle_swarm_optimization
+    """
+    pass
