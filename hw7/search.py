9bf11d598de7d410d765d8ed05951a142beffdf3
Jim Witschey <jim.witschey@gmail.com>
2014-10-26 01:38:19 -0400

    calculates shortnames at search time

--- hw7/search.py
+++ hw7/search.py
@@ -11,6 +11,9 @@ from witschey.rdiv import rdiv_report
 
 import timeit
 
+shortnames = {cls: filter(str.isupper, cls.__name__)
+              for cls in Searcher.__subclasses__()}
+
 def run(n=30, text_report=True):
     ms = Model.__subclasses__()
     ms.remove(models.Schwefel)
@@ -25,7 +28,7 @@ def run(n=30, text_report=True):
         for searcher_cls in ss:
             for _ in xrange(n):
                 Output = namedtuple('Output', ('name', 'best'))
-                name = ('{}'.format(searcher_cls.shortname))
+                name = ('{}'.format(shortnames[searcher_cls]))
                 searcher = searcher_cls(model_cls)
                 out = searcher.run(text_report=text_report)
                 if not hasattr(out, 'searcher'):
