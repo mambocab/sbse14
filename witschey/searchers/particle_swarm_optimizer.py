cc10d438a045f66c26f4e7d68f4217cbf1679918
Jim Witschey <jim.witschey@gmail.com>
2014-10-26 01:41:27 -0400

    implements PSO

--- witschey/searchers/particle_swarm_optimizer.py
+++ witschey/searchers/particle_swarm_optimizer.py
@@ -1,8 +1,12 @@
 from __future__ import division, print_function
 
 import random
+from itertools import izip
 
-from searcher import Searcher
+from searcher import Searcher, SearchReport
+from witschey.base import memo_sqrt as sqrt
+from witschey.log import NumberLog
+from witschey.models import ModelInputException
 
 
 def _random_scaled_velocity(a, b, scale=.1):
@@ -18,7 +22,50 @@ class ParticleSwarmOptimizer(Searcher):
     information, see https://github.com/timm/sbse14/wiki/pso#details and
     http://en.wikipedia.org/wiki/Particle_swarm_optimization
     """
-    pass
+
+    def __init__(self, *args, **kwargs):
+        super(ParticleSwarmOptimizer, self).__init__(*args, **kwargs)
+
+        self._flock = tuple(self._new_particle()
+                            for _ in range(self.spec.population_size))
+        self._evals = len(self._flock)
+        self._current_flock_energies = NumberLog(p.energy
+                                                 for p in self._flock)
+        self._best = min(self._flock, key=lambda x: x.energy)
+        self._lives = 4
+
+    def _new_particle(self):
+        return Particle(self.model, self.spec.phi1, self.spec.phi2)
+
+    def _update(self):
+        self._prev_flock_energies = self._current_flock_energies
+        self._flock = tuple(p._update(self._best) for p in self._flock)
+        self._evals += len(self._flock)
+
+        self._current_flock_energies = NumberLog(p.energy
+                                                 for p in self._flock)
+
+        self._best = min(self._best, *self._current_flock_energies,
+                         key=lambda x: x.energy)
+        if self._current_flock_energies.better(self._prev_flock_energies):
+            self._best_flock = self._flock
+        else:
+            self._lives -= 1
+
+    def run(self, text_report=False):
+        for _ in range(self.spec.generations):
+            self._update()
+            if self._lives <= 0:
+                break
+
+        best_flock_energies = NumberLog(p.energy for p in self._best_flock)
+
+        return SearchReport(best=self._best,
+                            best_era=best_flock_energies,
+                            evaluations=self._evals,
+                            searcher=self.__class__,
+                            spec=self.spec,
+                            report=None)
 
 
 class Particle(object):
@@ -26,13 +73,48 @@ class Particle(object):
     A particle in the "flock".
     """
 
-    def __init__(self, model):
+    def __init__(self, model, phi1, phi2):
         self._model = model
-        self._loc = model.random_model_io()
-        self._best = self._io
+        self._current = model.random_model_io()
+        self._best = self._current
+        self._phi1, self._phi1 = phi1, phi2
 
-        self._velocites = tuple(_random_scaled_velocity(iv.lo, iv.hi)
-                                for iv in model.xs)
+        # calculate constriction factor
+        phi = phi1 + phi2
+        self._k = 2 / abs(2 - phi - sqrt(phi * phi) - 4 * phi)
 
-    def update(self, local_best):
-        pass
+        # initialize velocities
+        self._velocity = tuple(_random_scaled_velocity(iv.lo, iv.hi)
+                               for iv in model.xs)
+
+    @property
+    def energy(self):
+        return self._current.energy
+
+    def _compute_new_velocity(self, local_best):
+        to_local = tuple(a - b
+                             for a, b in izip(local_best, self._current.xs))
+        to_personal = tuple(a - b
+                            for a, b in izip(self._best.xs, self._current.xs))
+        v = tuple((self._k * (v + self._phi1 * loc + self._phi2 * pers)
+                   for v, loc, pers in izip(
+                       self._velocity, to_local, to_personal)))
+        return v
+
+    def _update(self, local_best):
+        """
+        Given local_best, the best value seen by this particle's neighbors,
+        find the particle's new velocity.
+        """
+        init_loc = self._current.xs
+        init_vel = self._velocity
+        candidate_xs = tuple(p + v for p, v in izip(init_loc, init_vel))
+
+        updated = False
+        while not updated:
+            try:
+                self._current = self.model.compute_model_io(candidate_xs)
+            except ModelInputException:
+                candidate_xs = self.model.random_replace(candidate_xs)
+
+        self._velocity = self._compute_new_velocity(self._best)
