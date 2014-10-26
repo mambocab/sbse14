bda04bbd01d386926941e8ec894cab43e774cba0
Jim Witschey <jim.witschey@gmail.com>
2014-10-25 22:01:40 -0400

    adds docstring with doctest to IV

--- witschey/models/independent_variable.py
+++ witschey/models/independent_variable.py
@@ -6,6 +6,26 @@ import random
 
 
 class IndependentVariable(object):
+    """
+    An independent variable for a model.
+
+    >>> iv = IndependentVariable(0, 10)
+    >>> iv.lo, iv.hi
+    (0, 10)
+
+    Call an independent variable object to generating random variables within
+    its range:
+
+    >>> random.seed(1); iv(), iv(), iv()
+    (1.3436424411240122, 8.474337369372327, 7.6377461897661405)
+
+    Provices a `clip` method to return a variable clipped within the bounds
+    of the variable:
+
+    >>> iv.clip(10.5), iv.clip(-100), iv.clip(4.2)
+    (10, 0, 4.2)
+    """
+
     def __init__(self, lo, hi, type=float):
         self._lo = lo
         self._hi = hi
@@ -20,6 +40,9 @@ class IndependentVariable(object):
         return self._get(self.lo, self.hi)
 
     def clip(self, x):
+        """
+        Clip the input number within the bounds of the independent variable.
+        """
         return max(self.lo, min(self.hi, x))
 
     @property
