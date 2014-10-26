4942a274151aca045badc8af7a33627ac58ac39c
Jim Witschey <jim.witschey@gmail.com>
2014-10-25 22:11:45 -0400

    init Particle class for PSO

--- witschey/searchers/particle_swarm_optimizer.py
+++ witschey/searchers/particle_swarm_optimizer.py
@@ -1,6 +1,13 @@
 from __future__ import division, print_function
 
+import random
+
 from searcher import Searcher
+
+
+def _random_scaled_velocity(a, b, scale=.1):
+    magnitude = max(a, b) - min(a, b)
+    return random.uniform(-magnitude, magnitude) * scale
 
 
 class ParticleSwarmOptimizer(Searcher):
@@ -12,3 +19,20 @@ class ParticleSwarmOptimizer(Searcher):
     http://en.wikipedia.org/wiki/Particle_swarm_optimization
     """
     pass
+
+
+class Particle(object):
+    """
+    A particle in the "flock".
+    """
+
+    def __init__(self, model):
+        self._model = model
+        self._loc = model.random_model_io()
+        self._best = self._io
+
+        self._velocites = tuple(_random_scaled_velocity(iv.lo, iv.hi)
+                                for iv in model.xs)
+
+    def update(self, local_best):
+        pass
