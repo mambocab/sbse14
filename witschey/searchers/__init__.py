5abb5ecfa70e7f80757768e1d6e41f29b1199c7d
Jim Witschey <jim.witschey@gmail.com>
2014-10-25 21:05:13 -0400

    searhcer shortnames in __init__ were a bad idea

--- witschey/searchers/__init__.py
+++ witschey/searchers/__init__.py
@@ -5,16 +5,6 @@ from genetic_algorithm import GeneticAlgorithm
 from differential_evolution import DifferentialEvolution
 from particle_swarm_optimizer import ParticleSwarmOptimizer
 
-SEARCHER_SHORTNAMES = {
-    SimulatedAnnealer:     'SA',
-    MaxWalkSat:            'MWS',
-    GeneticAlgorithm:      'GA',
-    DifferentialEvolution: 'DE'
-}
-
-for searcher_cls, shortname in SEARCHER_SHORTNAMES.iteritems():
-    searcher_cls.shortname = shortname
-
 __all__ = [Searcher, SearcherConfig, SearchReport,
            SimulatedAnnealer, MaxWalkSat,
            GeneticAlgorithm, DifferentialEvolution, ParticleSwarmOptimizer]
